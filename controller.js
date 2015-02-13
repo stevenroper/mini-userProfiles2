cc5d37a4d28a96f7675f7c8524e5c88d75f816b8
Steven Roper <steven.m.roper@gmail.com>
2015-02-12 19:29:58 -0700

    Setting up promises using .

--- controller.js
+++ controller.js
@@ -3,9 +3,10 @@ var app = angular.module('userProfiles');
 app.controller('MainController', function($scope, mainService) {
 
   $scope.getUsers = function() {
-  	$scope.users = mainService.getUsers();
+  	mainService.getUsers().then(function(users) {
+  		$scope.users = users;
+  	});
   }
-
 
   $scope.getUsers();
 
--- service.js
+++ service.js
@@ -1,12 +1,20 @@
-var app = angular.module('userProfiles'); //wow
+var app = angular.module('userProfiles');
 
 app.service('mainService', function($http, $q) {
 
   this.getUsers = function() {
-    return $http({
+    var deferred = $q.defer();
+
+    $http({
       method: 'GET',
       url: 'http://reqr.es/api/users?page=1'
+    }).then(function(response) {
+      deferred.resolve(response.data.data);
+    }, function(error) {
+      deferred.reject(error)
     });
+
+    return deferred.promise;
   };
 
 
